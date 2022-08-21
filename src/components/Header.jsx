3e2792316a957aee8391ea7434bf0e0e9d2f336d
elvin <elvinyeka@gmail.com>
2022-08-21 20:42:29 +0400

    header:  logo ready & links start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import logo from "../assets/images/emilya-logo-3.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <img src={logo} alt="emilya ismayilova logo" />
+        </div>
+        <nav className="header__nav">
+          <ul className="header__nav-links">
+            <li className="header__nav-link">
+              <NavLink
+                to="/"
+                className={(isActive) => (!isActive ? "active" : "")}
+              >
+                home
+              </NavLink>
+            </li>
+            <li className="header__nav-link">
+              <NavLink
+                to="/"
+                className={(isActive) => (!isActive ? "active" : "")}
+              >
+                cake baku
+              </NavLink>
+            </li>
+            <li className="header__nav-link">
+              <NavLink
+                to="/"
+                className={(isActive) => (!isActive ? "active" : "")}
+              >
+                cake school
+              </NavLink>
+            </li>
+            <li className="header__nav-link">
+              <NavLink
+                to="/"
+                className={(isActive) => (!isActive ? "active" : "")}
+              >
+                catering baku
+              </NavLink>
+            </li>
+            <li className="header__nav-link">
+              <a href="#contact">contact</a>
+            </li>
+          </ul>
+          <button className="header__nav-btn">
+            <FaBars />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
