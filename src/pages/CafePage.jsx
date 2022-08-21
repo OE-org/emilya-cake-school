5093e5a96b81ed789f5e166b2da4e53d10aaa728
elvin <elvinyeka@gmail.com>
2022-08-21 19:40:25 +0400

    refactoring:  folder structure and starter files created

---
+++ src/pages/CafePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CafePage = () => {
+  return <div>CafePage</div>;
+};
+
+export default CafePage;
---
+++ src/pages/CakeSchool.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CakeSchool = () => {
+  return <div>CakeSchool</div>;
+};
+
+export default CakeSchool;
