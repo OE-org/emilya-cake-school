2f84772ce98ad0fc8801463e285e7f73d80fd40d
elvin <elvinyeka@gmail.com>
2022-08-22 22:58:48 +0400

    hero section first design ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import hero from "../assets/images/hero-3.png";
+import cakeBaku from "../assets/images/services/cake-baku.png";
+import cakeSchool from "../assets/images/services/cake-school.png";
+import catering from "../assets/images/services/catering-baku.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="hero">
+        <div className="hero__wrapper">
+          <div className="hero__img">
+            <img src={hero} alt="emilya ismayilova" />
+          </div>
+          <div className="hero__content">
+            <h1>I'm Emilya Ismayilova</h1>
+            <div className="hero__content-cards">
+              <Link to="/cake-baku" className="hero__card-wrapper">
+                <div className="hero__card">
+                  <img src={cakeBaku} alt="cake baku" />
+                  <p>Cake Baku</p>
+                </div>
+              </Link>
+              <Link to="/cake-school" className="hero__card-wrapper">
+                <div className="hero__card">
+                  <img src={cakeSchool} alt="cake school" />
+                  <p>Cake School</p>
+                </div>
+              </Link>
+              <Link to="/catering" className="hero__card-wrapper">
+                <div className="hero__card">
+                  <img src={catering} alt="catering baku" />
+                  <p>Cake Baku</p>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
