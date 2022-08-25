e64c63dbdba9404ff1b2f8f81e866c9fbf997435
elvin <elvinyeka@gmail.com>
2022-08-25 23:27:41 +0400

    about me section ready

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,46 +9,50 @@ const About = () => {
     <div className="container">
       <div className="about__wrapper">
         <div className="about__info">
-          <div className="about__info-wrapper">
-            <h2 className="about__info-title">
-              <span>Emilya Ismayova</span> kimdir?
-            </h2>
-            <p className="about__info-desc">
-              <strong>Ad:</strong> Emilya Ismayilova
-            </p>
-            <p className="about__info-desc">
-              <strong>Doğum ili:</strong> 1990 ci il 16 iyun Borçalı mahalının
-              Marneuli rayonu
-            </p>
-            <p className="about__info-desc">
-              <strong>Orta təhsil:</strong> 1996-ci ildə Bakı şəhəri Xətai
-              rayonu 204 nömrəli orta məktəbdə oxumağa başlamışdır
-            </p>
-            <p className="about__info-desc">
-              <strong>Təhsili:</strong> 2008 ci ildə Azərbaycan Dövlət İqtisad
-              Universitetinin Biznesin təşkili ve idarəedilmesi fakultəsinə
-              qəbul olmuş və 2012 ci ildə universiteti yüksək balla bitirərək
-              Diplom əldə etmişdir !
-            </p>
-            <h4 className="about__info-subtitle">Həyatı və Biznesi haqqında</h4>
-            <p className="about__info-subdesc">
-              2012 ci ildən etibarən öz biznesini quraraq, 2012-2013 cü illərdə
-              hal hazırda da fəaliyyət göstərən Cake Baku Şirniyyat evinin təsis
-              etmişdir!
-            </p>
-            <p className="about__info-subdesc">
-              Fəaliyyətə başladığı illərdən həmdə şirniyyat evinə kadrlar
-              hazırlayaraq öz şəxsi biznesini dahada genişləndirmişdir.
-            </p>
-            <p className="about__info-subdesc">
-              Tələbələrin və təlabatin çoxluğundan qərara gələrək hal hazırda da
-              fəaliyyət göstərən Cake School adlı kulinariya məktəbini təsis
-              etmişdir!
-            </p>
-            <p className="about__info-subdesc">
-              Bununla yanaşı Catering Baku adlı keytring şirkətinin təsisçi
-              Direktoru Emilya İsmayilova ailəlidir və 2 övlad anasıdır !
-            </p>
+          <div className="about__info-bg">
+            <div className="about__info-wrapper">
+              <h2 className="about__info-title">
+                <span>Emilya Ismayova</span> kimdir?
+              </h2>
+              <p className="about__info-desc">
+                <strong>Ad:</strong> Emilya Ismayilova
+              </p>
+              <p className="about__info-desc">
+                <strong>Doğum ili:</strong> 1990 ci il 16 iyun Borçalı mahalının
+                Marneuli rayonu
+              </p>
+              <p className="about__info-desc">
+                <strong>Orta təhsil:</strong> 1996-ci ildə Bakı şəhəri Xətai
+                rayonu 204 nömrəli orta məktəbdə oxumağa başlamışdır
+              </p>
+              <p className="about__info-desc">
+                <strong>Təhsili:</strong> 2008 ci ildə Azərbaycan Dövlət İqtisad
+                Universitetinin Biznesin təşkili ve idarəedilmesi fakultəsinə
+                qəbul olmuş və 2012 ci ildə universiteti yüksək balla bitirərək
+                Diplom əldə etmişdir !
+              </p>
+              <h4 className="about__info-subtitle">
+                Həyatı və Biznesi haqqında
+              </h4>
+              <p className="about__info-subdesc">
+                2012 ci ildən etibarən öz biznesini quraraq, 2012-2013 cü
+                illərdə hal hazırda da fəaliyyət göstərən Cake Baku Şirniyyat
+                evinin təsis etmişdir!
+              </p>
+              <p className="about__info-subdesc">
+                Fəaliyyətə başladığı illərdən həmdə şirniyyat evinə kadrlar
+                hazırlayaraq öz şəxsi biznesini dahada genişləndirmişdir.
+              </p>
+              <p className="about__info-subdesc">
+                Tələbələrin və təlabatin çoxluğundan qərara gələrək hal hazırda
+                da fəaliyyət göstərən Cake School adlı kulinariya məktəbini
+                təsis etmişdir!
+              </p>
+              <p className="about__info-subdesc">
+                Bununla yanaşı Catering Baku adlı keytring şirkətinin təsisçi
+                Direktoru Emilya İsmayilova ailəlidir və 2 övlad anasıdır !
+              </p>
+            </div>
           </div>
         </div>
         <div className="about__carousel">
