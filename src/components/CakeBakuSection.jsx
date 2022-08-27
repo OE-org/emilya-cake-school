23394b946bb3b212627c54c022a4437c64ad2db5
elvin <elvinyeka@gmail.com>
2022-08-27 17:47:07 +0400

    cake baku section second look

--- src/components/CakeBakuSection.jsx
+++ src/components/CakeBakuSection.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper";
 
-import { cakeBakuImgs } from "../data";
 import { Link } from "react-router-dom";
 
 const CakeBakuSection = () => {
