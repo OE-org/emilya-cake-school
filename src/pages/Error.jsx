30d2f5c58729ddf9817462f96c1cbc48582fb004
elvin <elvinyeka@gmail.com>
2022-09-21 12:10:09 +0400

    error page style done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div className="error">Error</div>;
+  return (
+    <div className="error">
+      <h2 className="error__title">404</h2>
+      <p className="error__info">Səhifə tapılmadı</p>
+      <div className="cake-baku__info-btn">
+        <Link className="error__back" to="/">
+          Ana səhifəyə get
+        </Link>
+      </div>
+    </div>
+  );
 };
+//.cake-baku__info-btn
 
 export default Error;
